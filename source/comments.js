38873618f5ff505677e6cc0b8e466206b91b434f
Mia Lehrer & GregoryW <pair+mllehrer.mryakamoto@devbootcamp.com>
2016-01-05 11:12:00 -0800

    finish jquery add comments drill

--- source/comments.js
+++ source/comments.js
@@ -1,4 +1,15 @@
 // Shorthand for document ready
-$(function(){
-  // Your code goes here
+$(document).ready(function(){
+  $("#new_comment_button").click(function(){
+    $("form#new_comment").toggle();
+  });
+  $("#submitComment").click(function(event){
+    event.preventDefault();
+    var name, text;
+    name = $("#authorName").val();
+    text = $("#commentText").val();
+    $("#comment_list").append("<li>" + text + "<span class='author'>" + name + "</span></li>");
+    $("form#new_comment")[0].reset();
+    $("form#new_comment").hide();
+  });
 });
